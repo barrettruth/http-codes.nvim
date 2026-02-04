af0fd7cf2eda73b90aa620b40471db5cc7d6477d
Barrett Ruth <br.barrettruth@gmail.com>
2026-02-03 21:06:00 -0500

    fix: suppress deprecation stack trace for cleaner UX

--- lua/http-codes.lua
+++ lua/http-codes.lua
@@ -70,7 +70,7 @@ M.http_codes = M.pick
 
 ---@deprecated Use `vim.g.http_codes` instead
 function M.setup(user_config)
-  vim.deprecate('require("http-codes").setup()', 'vim.g.http_codes', 'v0.1.0', 'http-codes.nvim')
+  vim.deprecate('require("http-codes").setup()', 'vim.g.http_codes', 'v0.1.0', 'http-codes.nvim', false)
 
   if user_config then
     vim.g.http_codes = vim.tbl_deep_extend('force', vim.g.http_codes or {}, user_config)

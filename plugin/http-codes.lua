42fb4b5b2a168be74b77c7869024836055c5f16f
Barrett Ruth <br.barrettruth@gmail.com>
2026-02-07 14:01:22 -0500

    fix(ci): run pre-commit

--- plugin/http-codes.lua
+++ plugin/http-codes.lua
@@ -7,4 +7,6 @@ vim.api.nvim_create_user_command('HTTPCodes', function()
   require('http-codes').pick()
 end, {})
 
-vim.keymap.set('n', '<Plug>(http-codes-pick)', function() require('http-codes').pick() end, { desc = 'Pick HTTP status code' })
+vim.keymap.set('n', '<Plug>(http-codes-pick)', function()
+  require('http-codes').pick()
+end, { desc = 'Pick HTTP status code' })
